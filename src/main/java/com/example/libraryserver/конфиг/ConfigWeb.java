acdbe7a474e90e6767946ffee52c5a43ed9ed8b3
1.kla$ <recum12233@mail.ru>
2024-04-04 22:03:19 +0300

    убрат сделат

--- src/main/java/com/example/libraryserver/конфиг/ConfigWeb.java
+++ src/main/java/com/example/libraryserver/конфиг/ConfigWeb.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-public class КонфигВеб implements WebMvcConfigurer {
+public class ConfigWeb implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
